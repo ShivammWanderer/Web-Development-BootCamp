2ab355d528bbfb2cf823d56e3bb941e28fef9452
ShivammWanderer <44729615+ShivammWanderer@users.noreply.github.com>
2021-06-21 00:58:51 +0530

    Commit

--- ToDoList Complete/app.js
+++ ToDoList Complete/app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
@@ -9,10 +8,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
