1195ed090e4fe5cab5e165f3f2e78a4fa54a7b96
ShivammWanderer <44729615+ShivammWanderer@users.noreply.github.com>
2021-06-20 14:57:37 +0530

    Commit

--- Blog-Complete/app.js
+++ Blog-Complete/app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
@@ -13,7 +12,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
